07b616d1adc19d6ef93a70d0f77761575d8c98e4
rbrownGithub <rodalebrownstiegleredtech@outlook.com>
2024-07-25 22:49:01 -0400

    Update BookProgram.java

--- src/main/java/org/example/BookProgram.java
+++ src/main/java/org/example/BookProgram.java
@@ -1,2 +1,41 @@
-package org.example;public class BookProgram {
+package org.example;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BookProgram {
+    public static void main(String[] args) {
+        ArrayList<Book> books = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Title: ");
+            String title = scanner.nextLine();
+            if (title.isEmpty()) {
+                break;
+            }
+
+            System.out.print("Pages: ");
+            int pages = Integer.parseInt(scanner.nextLine());
+
+            System.out.print("Publication year: ");
+            int year = Integer.parseInt(scanner.nextLine());
+
+            books.add(new Book(title, pages, year));
+        }
+
+        System.out.print("What information will be printed? ");
+        String printOption = scanner.nextLine();
+
+        if (printOption.equalsIgnoreCase("everything")) {
+            for (Book book : books) {
+                System.out.println(book);
+            }
+        } else if (printOption.equalsIgnoreCase("name")) {
+            for (Book book : books) {
+                System.out.println(book.getTitle());
+            }
+        }
+
+        scanner.close();
+    }
 }
