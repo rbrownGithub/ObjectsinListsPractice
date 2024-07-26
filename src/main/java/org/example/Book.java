11f3b3cab88aadf718ac685573a4ba3c5a6c5363
rbrownGithub <rodalebrownstiegleredtech@outlook.com>
2024-07-25 22:48:06 -0400

    Update Book.java

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -1,2 +1,32 @@
-package org.example;public class Book {
+package org.example;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Book {
+    private String title;
+    private int pages;
+    private int publicationYear;
+
+    public Book(String title, int pages, int publicationYear) {
+        this.title = title;
+        this.pages = pages;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return getTitle() + ", " + getPages() + " pages, " + getPublicationYear();
+    }
 }
