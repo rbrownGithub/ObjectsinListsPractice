106dcd58cbf9f8337beb341f1eb54aa1544cbe80
rbrownGithub <rodalebrownstiegleredtech@outlook.com>
2024-07-25 22:47:35 -0400

    Update AnimalProgram.java

--- src/main/java/org/example/AnimalProgram.java
+++ src/main/java/org/example/AnimalProgram.java
@@ -1,7 +1,32 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Main {
+public class AnimalProgram {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Animal> animals = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Animal Menu");
+        System.out.println("Entering nothing will stop the loop");
+
+        while (true) {
+            System.out.print("Please enter a name:");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+
+            System.out.print("Is it a dog? Yes or no:");
+            boolean isDog = scanner.nextLine().equalsIgnoreCase("yes");
+
+            animals.add(new Animal(name, isDog));
+        }
+
+        for (Animal animal : animals) {
+            System.out.println(animal);
+        }
+
+        scanner.close();
     }
 }
