92f2723b879af112826d3a418b8835366fa5fe41
rbrownGithub <rodalebrownstiegleredtech@outlook.com>
2024-07-25 22:50:41 -0400

    Update TvShowProgram.java

--- src/main/java/org/example/TvShowProgram.java
+++ src/main/java/org/example/TvShowProgram.java
@@ -1,2 +1,32 @@
-package org.example;public class TvShowProgram {
+package org.example;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TvShowProgram {
+    public static void main(String[] args) {
+        ArrayList<TvShow> tvShows = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Name of the show: ");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+
+            System.out.print("How many episodes? ");
+            int episodes = Integer.parseInt(scanner.nextLine());
+
+            System.out.print("What is the genre? ");
+            String genre = scanner.nextLine();
+
+            tvShows.add(new TvShow(name, episodes, genre));
+        }
+
+        for (TvShow show : tvShows) {
+            System.out.println(show);
+        }
+
+        scanner.close();
+    }
 }
