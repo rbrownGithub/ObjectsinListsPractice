f69678d32ba2a8971c40e5ebfd5a6738a97396ff
rbrownGithub <rodalebrownstiegleredtech@outlook.com>
2024-07-25 22:46:56 -0400

    Update Animal.java

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -1,2 +1,26 @@
-package org.example;public class Animal {
+package org.example;package org.example;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Animal {
+    private String name;
+    private boolean isDog;
+
+    public Animal(String name, boolean isDog) {
+        this.name = name;
+        this.isDog = isDog;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isDog() {
+        return isDog;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + " is a dog = " + isDog();
+    }
 }
