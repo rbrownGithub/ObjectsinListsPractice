f3a3089afa07f99c0093ac114aacd4a3e759e94c
rbrownGithub <rodalebrownstiegleredtech@outlook.com>
2024-07-25 22:49:49 -0400

    Update TVShow.java

--- src/main/java/org/example/TVShow.java
+++ src/main/java/org/example/TVShow.java
@@ -1,2 +1,33 @@
-package org.example;public class TVShow {
+package org.example;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class TvShow {
+    private String name;
+    private int numberOfEpisodes;
+    private String genre;
+
+    public TvShow(String name, int numberOfEpisodes, String genre) {
+        this.name = name;
+        this.numberOfEpisodes = numberOfEpisodes;
+        this.genre = genre;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfEpisodes() {
+        return numberOfEpisodes;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    @Override
+    public String toString() {
+        return "The name of the show is " + getName() + " with " + getNumberOfEpisodes() + " number of episodes. The genre is " + getGenre();
+    }
 }
+
